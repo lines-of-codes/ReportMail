a558f2c0bb2559d0e2ebbcece0080e2ba4cf612f
Satakun Utama <49134472+lines-of-codes@users.noreply.github.com>
2021-07-02 23:05:10 +0700

    Add .gitignore

--- commands/close.js
+++ commands/close.js
@@ -2,6 +2,9 @@ module.exports = {
 	name: "close",
 	description: "Close a ticket.",
 	execute(message, args, Discord){
-		
+		if(args[0] == undefined) {
+			message.reply("Please provide the ID of the Ticket you wanted to delete.");
+		}
+		var query = {id: args[0]};
 	}
 }
